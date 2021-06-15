6c8af8d3ac3e4a24d10d03f30fafdb359cb39500
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-06-15 15:08:46 +0500

    Show more info button

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
-import react from "react";
+import React, { useState, useEffect } from "react";
 
 const Tour = ({ id, image, info, name, price }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
@@ -9,7 +10,15 @@ const Tour = ({ id, image, info, name, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore
+            ? info //if readMore = true then show info, else
+            : `${info.substring(0, 250)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show less" : "Show more"}
+          </button>
+        </p>
+
         <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
