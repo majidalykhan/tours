5bd918cbb1594d62b3a2f3bc2016262db022660f
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-06-15 14:44:31 +0500

    Tours fetch data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
@@ -16,7 +34,11 @@ function App() {
     );
   }
 
-  return <h2>Tours Project</h2>;
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Tours.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Tours = () => {
+  return (
+    <div className="tour">
+      <h2>Tours Component</h2>
+    </div>
+  );
+};
+
+export default Tours;
