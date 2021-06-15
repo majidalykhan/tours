c73267b880950425d20f34288ae8f0eeb9056dc5
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-06-15 14:31:46 +0500

    Loading component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return <h2>Tours Project</h2>;
 }
 
